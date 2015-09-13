dc97f433c2e91970dae225d6c5f52b90b0a78d37
Mart <martineq@gmail.com>
2015-09-13 01:12:05 -0300

    Agrego tests y coveralls-cmake a CMakeLists. Agrego travis.

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,15 @@
 // test.cpp
- 
-int main() {
+
+#include "../src/LibraryTest.h"
+
+int main(int argc, char **argv) {
+
+    LibraryTest lt;
+    lt.EjemploRocksDB();
+    //lt.EjemploMongoose();
+    lt.EjemploJson();
+
+    // Test siempre positivo (Sólo de prueba)
     return 0;
 }
+
