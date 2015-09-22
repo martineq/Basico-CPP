f132b80df5d1d64d068d8806a42d1d10e721a0fc
Mart <martineq@gmail.com>
2015-09-21 21:39:17 -0300

    Saco test de RocksDB

--- test/test.cpp
+++ test/test.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(int argc, char **argv) {
 
     LibraryTest lt;
-    lt.EjemploRocksDB();
+    //lt.EjemploRocksDB();
     lt.EjemploMongoose();
     lt.EjemploJson();
 
