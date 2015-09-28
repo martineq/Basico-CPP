0b8a96c4b06628dfb7255e56fca74accd77b0016
Mart <martineq@gmail.com>
2015-09-28 19:30:52 -0300

    Cambio CMake y modifico otros archivos para seguie el estandar

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+#include    <iostream>
+#include    "helloworld.h"
+
+using namespace std;
+ 
+int main(int argc, char** argv) {
+
+  HelloWorld hw;
+  hw.EjemploRocksDB();
+  hw.EjemploMongoose();
+  hw.EjemploJson();
+
+	return 0;
+}
+
